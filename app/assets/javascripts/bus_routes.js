c232065c8da3cc41035de566372cd4eaa62e6469
PhamKhanh <phamkhanh90@gmail.com>
2013-11-16 21:37:29 +0700

    finish part show multi parts bus routes

--- app/assets/javascripts/bus_routes.js
+++ app/assets/javascripts/bus_routes.js
@@ -13,12 +13,28 @@ function drawPathGoogleRoutes(choose_routes_bus, choose_true_or_false, mypath) {
   });
 }
 
-function setMarkerCoordinateMap(choose_routes_bus, choose_true_or_false) {
+function setMarkerCoordinateMap(choose_routes_bus, choose_true_or_false, markerList) {
   $.get("/bus_way", {choose_routes_bus: choose_routes_bus, choose_true_or_false: choose_true_or_false, flag: true},
   function(data, status) {
     if (status == "success") {
+      document.getElementById("divInfoOfRoute").innerHTML = "";
       for (var i = 0; i < data["TABLE"][0]["ROW"].length; i += 1) {
+        var utmInter = new UTMRef(data["TABLE"][0]["ROW"][i]["COL"][3]["DATA"], data["TABLE"][0]["ROW"][i]["COL"][4]["DATA"], "N", 48);
+        var llInter = utmInter.toLatLng();
+        var latLngInter = new google.maps.LatLng(llInter.lat, llInter.lng);
+        document.getElementById('divInfoOfRoute').innerHTML += "<div class=\"report\" onclick=\"moveTo(" + llInter.lat + ", " + llInter.lng + ",'" + data["TABLE"][0]["ROW"][i]["COL"][0]["DATA"]
+        + ' ' + data["TABLE"][0]["ROW"][i]["COL"][1]["DATA"] + ' Quận ' + data["TABLE"][0]["ROW"][i]["COL"][2]["DATA"] + "');\">" +
+          "[" + i + "][" +  data["TABLE"][0]["ROW"][i]["COL"][7]["DATA"] + "]" +
+        data["TABLE"][0]["ROW"][i]["COL"][0]["DATA"] + ' ' + data["TABLE"][0]["ROW"][i]["COL"][1]["DATA"] + ' Quận ' + data["TABLE"][0]["ROW"][i]["COL"][2]["DATA"] + '</div>';
+
+        var marker = new google.maps.Marker({
+          position: latLngInter,
+          title: data["TABLE"][0]["ROW"][i]["COL"][0]["DATA"] + ' ' + data["TABLE"][0]["ROW"][i]["COL"][1]["DATA"] + 'District' + data["TABLE"][0]["ROW"][i]["COL"][2]["DATA"],
+          map: map
+        });
+        markerList.push(marker);
       }
+      map.panTo(markerList[0].getPosition());
     }
   });
 }
@@ -63,7 +79,7 @@ $(function() {
     });
 
     // Function process set marker coordinate to map
-    setMarkerCoordinateMap(choose_routes_bus, choose_true_or_false);
+    setMarkerCoordinateMap(choose_routes_bus, choose_true_or_false, markerList);
 
     // Function process get data coordinate lat and long
     drawPathGoogleRoutes(choose_routes_bus, choose_true_or_false, mypath);
