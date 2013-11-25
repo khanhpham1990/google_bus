304ceae25967ab80f364d27e6b385c297c503815
khanhpn2013 <khanhpn@zigexn.vn>
2013-11-25 16:56:35 +0700

    finish sidekiq crawl data

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,22 +1,23 @@
 class HardWorker
   include Sidekiq::Worker
+  require 'net/http'
 
   def perform
-    i = 0
-    until i <= 160 do
+    i = 1
+    until i >= 160 do
       create_file_bus_station(i, true)
-      create_file_bus_station(i,false)
+      create_file_bus_station(i, false)
       i += 1
     end
-    binding.pry
   end
 
   def create_file_bus_station(tuyen_id, luotdi)
     response = Typhoeus::Request.post("http://map.ebms.vn/Server.aspx?action=GetTuyen", :params => {:TuyenId => tuyen_id, :LuotDi => luotdi})
-    if luotdi == true
-      File.open(Rails.root.to_s + "/public/data_maps/getAllBuses/#{luotdi}/GetTuyenBus_#{luotdi}#{tuyen_id}.json", "w") {|f| f.write(JSON.parse(response.response_body))}
-    else
-      File.open(Rails.root.to_s + "/public/data_maps/getAllBuses/#{luotdi}/GetTuyenBus_#{luotdi}#{tuyen_id}.json", "w") {|f| f.write(JSON.parse(response.response_body))}
+    if luotdi == true && !response.response_body.blank?
+      File.open(Rails.root.to_s + "/public/data_maps/getAllBuses/#{luotdi}/GetTuyenBus_#{luotdi}#{tuyen_id}.json", "w:UTF-8") {|f| f.write(response.response_body.force_encoding("UTF-8"))}
+    end
+    if luotdi == false && !response.response_body.blank?
+      File.open(Rails.root.to_s + "/public/data_maps/getAllBuses/#{luotdi}/GetTuyenBus_#{luotdi}#{tuyen_id}.json", "w:UTF-8") {|f| f.write(response.response_body.force_encoding("UTF-8"))}
     end
   end
 
